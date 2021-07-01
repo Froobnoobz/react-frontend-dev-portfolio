1f145ad77b5d9efa600872ddfc0f37b820575a38
Ben Perram <ben.perram2@gmail.com>
2021-07-01 21:22:59 +1000

    Updates

---
+++ precache-manifest.acdf7fdd207d650df2d95d1e6a4ecbfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590e83c56a0b568b66c08c4f6892e661",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "03d71e0208d7540ee34e",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "bc210cd38daec563ad81",
+    "url": "/react-frontend-dev-portfolio/static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "03d71e0208d7540ee34e",
+    "url": "/react-frontend-dev-portfolio/static/js/2.2c75961d.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.2c75961d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc210cd38daec563ad81",
+    "url": "/react-frontend-dev-portfolio/static/js/main.be8cccc9.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bbcda29a5e00503cb014667a1278d467.js"
+  "/react-frontend-dev-portfolio/precache-manifest.acdf7fdd207d650df2d95d1e6a4ecbfe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
